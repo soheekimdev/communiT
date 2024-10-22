25e6fb6f98e716c993f3555ef7726df375673e1a
Kwonyeojiny <kangagi0009@naver.com>
2024-10-22 23:56:53 +0900

    Design: Add find-password

---
+++ src/components/cards/find-password/find-password-email.tsx
@@ -0,0 +1,26 @@
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter
+} from "@/components/ui/card"
+
+
+
+export function FindPasswordEmail() {
+  return (
+    <Card>
+      <CardContent className="text-center mt-16">
+        <CardDescription>
+          <div className="font-bold text-lg mb-4">m@example.com</div>
+          입력하신 이메일로 비밀번호 재설정 메일이 발송되었습니다
+        </CardDescription>
+      </CardContent>
+      <br/>
+      <CardFooter>
+        <Button className="w-full">확인</Button>
+      </CardFooter>
+    </Card>
+  );
+}
