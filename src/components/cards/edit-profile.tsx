578f957f98414a57958b4e55d28c81404621b8a6
Ju-Min-Jae <juminje@daum.net>
2024-10-23 18:17:28 +0900

    Refactor: form validation using react-hook-form and zod

--- src/components/cards/edit-profile.tsx
+++ src/components/cards/edit-profile.tsx
@@ -1,3 +1,4 @@
+import { useState, useRef } from 'react';
 import { Button } from '@/components/ui/button';
 import {
   Card,
@@ -9,7 +10,6 @@ import {
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui/avatar';
-import { useState, useRef } from 'react';
 import {
   User,
   Mail,
@@ -20,15 +20,36 @@ import {
   CheckCircle,
   XCircle,
 } from 'lucide-react';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import z from 'zod';
+
+const formSchema = z
+  .object({
+    userName: z
+      .string()
+      .trim()
+      .nonempty('이름을 입력해 주세요.')
+      .regex(/^[a-zA-Z0-9]*$/, '특수문자는 사용할 수 없습니다.')
+      .min(2, '이름은 최소 2자 이상이어야 합니다.'),
+    newPassword: z
+      .string()
+      .min(8, '비밀번호는 최소 8자 이상이어야 합니다.')
+      .regex(/^(?!.*(.)\1\1).*$/, '같은 문자를 3번 이상 반복할 수 없습니다.'),
+    confirmPassword: z.string(),
+  })
+  .refine((data) => data.newPassword === data.confirmPassword, {
+    message: '비밀번호가 일치하지 않습니다.',
+    path: ['confirmPassword'],
+  });
+
+type FormData = z.infer<typeof formSchema>;
 
 const EditProfile = () => {
   const [profileImage, setProfileImage] = useState<string>('');
-  const [userName, setUserName] = useState<string>('');
-  const [email, setEmail] = useState<string>('');
-  const [password, setPassword] = useState<string>('');
-  const [confirmPassword, setConfirmPassword] = useState<string>('');
-  const [errors, setErrors] = useState<{ [key: string]: string }>({});
-  const [showPassword, setShowPassword] = useState<boolean>(false);
+  const [showCurrentPassword, setShowCurrentPassword] =
+    useState<boolean>(false);
+  const [showNewPassword, setShowNewPassword] = useState<boolean>(false);
   const [showConfirmPassword, setShowConfirmPassword] =
     useState<boolean>(false);
 
@@ -36,20 +57,17 @@ const EditProfile = () => {
 
   const defaultImage = 'https://example.com/my-default-image.png';
 
-  const validateFields = () => {
-    const newErrors: { [key: string]: string } = {};
-    if (!email.includes('@'))
-      newErrors.email = '유효한 이메일 주소를 입력하세요.';
-    if (userName.length < 3)
-      newErrors.userName = '이름은 최소 3자 이상이어야 합니다.';
-    if (password.length < 6)
-      newErrors.password = '비밀번호는 최소 6자 이상이어야 합니다.';
-    if (password !== confirmPassword)
-      newErrors.confirmPassword = '비밀번호가 일치하지 않습니다.';
-    setErrors(newErrors);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    watch,
+    reset,
+  } = useForm<FormData>({
+    resolver: zodResolver(formSchema),
+  });
 
-    return Object.keys(newErrors).length === 0;
-  };
+  const userNameValue = watch('userName', '');
 
   const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0];
@@ -62,28 +80,18 @@ const EditProfile = () => {
     }
   };
 
-  const handleSubmit = (event: React.FormEvent) => {
-    event.preventDefault();
+  const onSubmit = (data: FormData) => {
+    // 프로필 수정 로직
 
-    if (validateFields()) {
-      // 프로필 수정 로직
-
-      console.log('프로필 수정 성공 : ', {
-        profileImage,
-        userName,
-        email,
-        password,
-      });
-    }
+    console.log('프로필 수정 성공 : ', {
+      profileImage,
+      ...data,
+    });
   };
 
   const handleCancel = () => {
     setProfileImage('');
-    setUserName('');
-    setEmail('');
-    setPassword('');
-    setConfirmPassword('');
-    setErrors({});
+    reset();
 
     if (imageFileRef.current) {
       imageFileRef.current.value = '';
@@ -97,7 +105,7 @@ const EditProfile = () => {
       </CardHeader>
 
       <CardContent>
-        <form onSubmit={handleSubmit} className="space-y-6">
+        <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
           <div className="flex flex-col items-center space-y-4">
             <div className="relative">
               <Avatar className="h-32 w-32 border-4 border-gray shadow-lg">
@@ -137,24 +145,12 @@ const EditProfile = () => {
                 <Input
                   id="email"
                   type="email"
-                  placeholder="userEmail@example.com"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  className={`pl-10 pr-10 ${
-                    errors.email ? 'border-red-500' : ''
-                  }`}
+                  value="Example@google.com"
+                  disabled
+                  className="pl-10 pr-10 bg-slate-300 text-green-700"
                 />
                 <Mail className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400 h-5 w-5" />
-                {email &&
-                  (errors.email ? (
-                    <XCircle className="absolute right-3 top-1/2 -translate-y-1/2 text-red-500 h-5 w-5" />
-                  ) : (
-                    <CheckCircle className="absolute right-3 top-1/2 -translate-y-1/2 text-green-500 h-5 w-5" />
-                  ))}
               </div>
-              {errors.email && (
-                <p className="text-red-500 text-xs mt-1">{errors.email}</p>
-              )}
             </div>
 
             <div className="relative">
@@ -166,37 +162,72 @@ const EditProfile = () => {
                   id="name"
                   type="text"
                   placeholder="userName"
-                  value={userName}
-                  onChange={(e) => setUserName(e.target.value)}
+                  {...register('userName')}
                   className={`pl-10 pr-10 ${
                     errors.userName ? 'border-red-500' : ''
                   }`}
                 />
                 <User className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400 h-5 w-5" />
-                {userName &&
-                  (errors.userName ? (
-                    <XCircle className="absolute right-3 top-1/2 -translate-y-1/2 text-red-500 h-5 w-5" />
-                  ) : (
+                {errors.userName ? (
+                  <XCircle className="absolute right-3 top-1/2 -translate-y-1/2 text-red-500 h-5 w-5" />
+                ) : (
+                  userNameValue.trim().length >= 2 && (
                     <CheckCircle className="absolute right-3 top-1/2 -translate-y-1/2 text-green-500 h-5 w-5" />
-                  ))}
+                  )
+                )}
               </div>
               {errors.userName && (
-                <p className="text-red-500 text-xs mt-1">{errors.userName}</p>
+                <p className="text-red-500 text-xs mt-1">
+                  {errors.userName.message}
+                </p>
+              )}
+            </div>
+
+            {/* 회원가입 기능 구현 완료 후 현재 비밀번호와 일치하는지 비교하는 기능 구현 예정 */}
+            <div className="relative">
+              <Label htmlFor="currentPassword" className="text-sm font-medium">
+                Current Password
+              </Label>
+              <div className="relative mt-1">
+                <Input
+                  id="currentPassword"
+                  value="회원가입 후 현재 비밀번호와 일치하는지 비교 예정(현재 disabled)"
+                  className="pl-10 pr-10 text-violet-900"
+                  disabled
+                />
+                <Lock className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400 h-5 w-5" />
+                <Button
+                  type="button"
+                  variant="ghost"
+                  size="icon"
+                  className="absolute right-1 top-1/2 -translate-y-1/2"
+                  onClick={() => setShowCurrentPassword(!showCurrentPassword)}
+                >
+                  {showCurrentPassword ? (
+                    <EyeOff className="h-5 w-5 text-gray-400" />
+                  ) : (
+                    <Eye className="h-5 w-5 text-gray-400" />
+                  )}
+                </Button>
+              </div>
+              {errors.newPassword && (
+                <p className="text-red-500 text-xs mt-1">
+                  {errors.newPassword.message}
+                </p>
               )}
             </div>
 
             <div className="relative">
-              <Label htmlFor="password" className="text-sm font-medium">
-                Password
+              <Label htmlFor="newPassword" className="text-sm font-medium">
+                New Password
               </Label>
               <div className="relative mt-1">
                 <Input
-                  id="password"
-                  type={showPassword ? 'text' : 'password'}
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
+                  id="newPpassword"
+                  type={showNewPassword ? 'text' : 'password'}
+                  {...register('newPassword')}
                   className={`pl-10 pr-10 ${
-                    errors.password ? 'border-red-500' : ''
+                    errors.newPassword ? 'border-red-500' : ''
                   }`}
                 />
                 <Lock className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400 h-5 w-5" />
@@ -205,17 +236,19 @@ const EditProfile = () => {
                   variant="ghost"
                   size="icon"
                   className="absolute right-1 top-1/2 -translate-y-1/2"
-                  onClick={() => setShowPassword(!showPassword)}
+                  onClick={() => setShowNewPassword(!showNewPassword)}
                 >
-                  {showPassword ? (
+                  {showNewPassword ? (
                     <EyeOff className="h-5 w-5 text-gray-400" />
                   ) : (
                     <Eye className="h-5 w-5 text-gray-400" />
                   )}
                 </Button>
               </div>
-              {errors.password && (
-                <p className="text-red-500 text-xs mt-1">{errors.password}</p>
+              {errors.newPassword && (
+                <p className="text-red-500 text-xs mt-1">
+                  {errors.newPassword.message}
+                </p>
               )}
             </div>
 
@@ -227,8 +260,7 @@ const EditProfile = () => {
                 <Input
                   id="confirm-password"
                   type={showConfirmPassword ? 'text' : 'password'}
-                  value={confirmPassword}
-                  onChange={(e) => setConfirmPassword(e.target.value)}
+                  {...register('confirmPassword')}
                   className={`pl-10 pr-10 ${
                     errors.confirmPassword ? 'border-red-500' : ''
                   }`}
@@ -250,7 +282,7 @@ const EditProfile = () => {
               </div>
               {errors.confirmPassword && (
                 <p className="text-red-500 text-xs mt-1">
-                  {errors.confirmPassword}
+                  {errors.confirmPassword.message}
                 </p>
               )}
             </div>
