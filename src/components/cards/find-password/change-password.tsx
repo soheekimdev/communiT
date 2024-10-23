c22d97a7b1510129937cd63375060c38d54a0e4d
Kwonyeojiny <kangagi0009@naver.com>
2024-10-24 00:35:00 +0900

    Refactor: Signin

--- src/components/cards/find-password/change-password.tsx
+++ src/components/cards/find-password/change-password.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@/components/ui/button";
+import { Button } from '@/components/ui/button';
 import {
   Card,
   CardContent,
@@ -6,36 +6,97 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+} from '@/components/ui/card';
+import { Lock, Eye, EyeOff } from 'lucide-react';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { useId, useState } from 'react';
 
 const ChangePassword = () => {
+  const [newPassword, setNewPassword] = useState<string>('');
+  const [confirmPassword, setConfirmPassword] = useState<string>('');
+  const [showNewPassword, setShowNewPassword] = useState<boolean>(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState<boolean>(false);
+  const [errors, setErrors] = useState<{ [key: string]: string }>({});
+
+  const newPasswordId = useId();
+  const newConfirmPasswordId = useId();
+
   return (
     <Card>
-      <CardHeader className="space-y-1 text-center">
+      <CardHeader className="space-y-1">
         <CardTitle className="text-2xl mb-2">비밀번호 변경</CardTitle>
-        <CardDescription>
-          변경할 비밀번호를 입력해주세요.
-        </CardDescription>
+        <CardDescription>변경할 비밀번호를 입력해주세요.</CardDescription>
       </CardHeader>
-      <br/>
       <CardContent className="grid gap-4">
-        <div className="grid gap-2">
-          <Label htmlFor="email">새 비밀번호</Label>
-          <Input id="password" type="password"/>
+        <div className="relative mb-2">
+          <div className="absolute inset-0 flex items-center">
+            <span className="w-full border-t" />
+          </div>
         </div>
-        <div className="grid gap-2">
-          <Label htmlFor="email">새 비밀번호 확인</Label>
-          <Input id="password" type="password"/>
-        </div>
+
+        <form>
+          <div className="relative mb-4">
+            <Label htmlFor={newPasswordId}>새 비밀번호</Label>
+            <div className="relative mt-1">
+              <Input
+                id={newPasswordId}
+                type={showNewPassword ? 'text' : 'password'}
+                value={newPassword}
+                onChange={e => setNewPassword(e.target.value)}
+                className={`pl-10 pr-10 ${errors.newpassword ? 'border-red-500' : ''}`}
+              />
+              <Lock className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400 h-5 w-5" />
+              <Button
+                type="button"
+                variant="ghost"
+                size="icon"
+                className="absolute right-1 top-1/2 -translate-y-1/2 hover:bg-transparent hover:text-inherit"
+                onClick={() => setShowNewPassword(!showNewPassword)}
+              >
+                {showNewPassword ? (
+                  <EyeOff className="h-5 w-5 text-gray-400" />
+                ) : (
+                  <Eye className="h-5 w-5 text-gray-400" />
+                )}
+              </Button>
+            </div>
+          </div>
+
+          <div className="relative mb-2">
+            <Label htmlFor={newConfirmPasswordId}>새 비밀번호 확인</Label>
+            <div className="relative mt-1">
+              <Input
+                id={newConfirmPasswordId}
+                type={showConfirmPassword ? 'text' : 'password'}
+                value={confirmPassword}
+                onChange={e => setConfirmPassword(e.target.value)}
+                className={`pl-10 pr-10 ${errors.confirmPassword ? 'border-red-500' : ''}`}
+              />
+              <Lock className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400 h-5 w-5" />
+              <Button
+                type="button"
+                variant="ghost"
+                size="icon"
+                className="absolute right-1 top-1/2 -translate-y-1/2 hover:bg-transparent hover:text-inherit"
+                onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+              >
+                {showConfirmPassword ? (
+                  <EyeOff className="h-5 w-5 text-gray-400" />
+                ) : (
+                  <Eye className="h-5 w-5 text-gray-400" />
+                )}
+              </Button>
+            </div>
+          </div>
+        </form>
       </CardContent>
-      <br/>
+      <br />
       <CardFooter>
-        <Button className="w-full">로그인 하러가기</Button>
+        <Button className="w-full">로그인</Button>
       </CardFooter>
     </Card>
   );
-}
+};
 
 export default ChangePassword;
--- src/components/cards/find-password/find-password-email.tsx
+++ src/components/cards/find-password/find-password-email.tsx
@@ -1,28 +1,22 @@
-import { Button } from "@/components/ui/button"
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter
-} from "@/components/ui/card"
-
-
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardDescription, CardFooter } from '@/components/ui/card';
 
 const FindPasswordEmail = () => {
   return (
     <Card>
       <CardContent className="text-center mt-16">
         <CardDescription>
-          <div className="font-bold text-lg mb-4">m@example.com</div>
+          <span className="font-bold text-lg mb-4">m@example.com</span>
+          <br />
           입력하신 이메일로 비밀번호 재설정 메일이 발송되었습니다
         </CardDescription>
       </CardContent>
-      <br/>
+
       <CardFooter>
         <Button className="w-full">확인</Button>
       </CardFooter>
     </Card>
   );
-}
+};
 
 export default FindPasswordEmail;
