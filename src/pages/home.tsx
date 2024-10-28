98bb9ae615693e028e54c8530d60e82f70ce41e1
Kwonyeojiny <kangagi0009@naver.com>
2024-10-28 15:31:12 +0900

    Chore: Remove minjae's component from home.tsx

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,4 @@
 import { cn } from '@/lib/utils';
-
-import MyProfile from '@/components/cards/my-profile';
-import EditProfile from '@/components/cards/edit-profile';
 import Signin from '@/components/cards/sign-in';
 import ChangePassword from '@/components/cards/find-password/change-password';
 import FindPassword from '@/components/cards/find-password/find-password';
@@ -17,16 +14,6 @@ function DemoContainer({ className, ...props }: React.HTMLAttributes<HTMLDivElem
 export default function HomePage() {
   return (
     <div className="grid items-start justify-center gap-6 rounded-lg p-8 md:grid lg:grid-cols-2 xl:grid-cols-3">
-      {/* 민재 컴포넌트 확인용 */}
-      <div className="col-span-2 grid items-start gap-6 lg:col-span-1">
-        <DemoContainer>
-          <MyProfile />
-        </DemoContainer>
-        <DemoContainer>
-          <EditProfile />
-        </DemoContainer>
-      </div>
-
       {/* 여진 컴포넌트 확인용 */}
       <div className="col-span-2 grid items-start gap-6 lg:col-span-1">
         <DemoContainer>
