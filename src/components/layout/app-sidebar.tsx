433c6853001cefa5fc02170fc6db54ed6ab00fb9
soheekimdev <soheekim.dev@gmail.com>
2024-11-05 11:05:49 +0900

    Feat: 로그인/로그아웃 로직에서 MenuItem 타입을 그대로 활용할 수 있도록 통합,  코드 일관성 유지

--- src/components/layout/app-sidebar.tsx
+++ src/components/layout/app-sidebar.tsx
@@ -1,4 +1,4 @@
-import { Home, Dumbbell, Users, LogOut, Settings, LucideIcon } from 'lucide-react';
+import { Home, Dumbbell, Users, LogIn, LogOut, Settings, LucideIcon } from 'lucide-react';
 import {
   Sidebar,
   SidebarContent,
@@ -7,12 +7,9 @@ import {
   SidebarGroupContent,
   SidebarHeader,
   SidebarMenu,
-  SidebarMenuButton,
   SidebarMenuItem,
-  useSidebar,
 } from '@/components/ui/sidebar';
 import SidebarLink from '../ui/sidebar-link';
-import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '@/RTK/store';
 import { logout } from '@/RTK/authSlice';
@@ -40,26 +37,37 @@ const mainMenuItems: MenuItem[] = [
   { type: 'link', title: '커뮤니티', to: '/posts', icon: Users },
 ];
 
-const subMenuItems: MenuItem[] = [
-  { type: 'link', title: '설정', to: '/settings', icon: Settings },
-  {
-    type: 'button',
-    title: '로그아웃',
-    icon: LogOut,
-    onClick: () => {
-      console.log('로그아웃 버튼 클릭'); // TODO: 로그아웃 로직에 연결
-    },
-  },
-];
-
 const AppSidebar = () => {
   const dispatch = useDispatch();
-  const { setOpenMobile } = useSidebar(); // useSidebar hook 사용
   const isLoggedIn = useSelector((state: RootState) => state.auth.isLoggedIn); // 로그인 상태 가져오기
 
   const handleLogout = () => {
     dispatch(logout());
   };
+
+  const authMenuItem: MenuItem = isLoggedIn
+    ? {
+        type: 'button',
+        title: '로그아웃',
+        icon: LogOut,
+        onClick: handleLogout,
+      }
+    : {
+        type: 'link',
+        title: '로그인',
+        to: '/sign-in',
+        icon: LogIn,
+      };
+
+  const subMenuItems: MenuItem[] = [
+    {
+      type: 'link',
+      title: '설정',
+      to: '/settings',
+      icon: Settings,
+    },
+    authMenuItem,
+  ];
 
   return (
     <Sidebar>
@@ -100,27 +108,6 @@ const AppSidebar = () => {
               )}
             </SidebarMenuItem>
           ))}
-          <SidebarMenuItem>
-            <SidebarMenuButton asChild>
-              {isLoggedIn ? (
-                <div
-                  className="flex items-center w-full gap-4 px-2 py-2 cursor-pointer"
-                  onClick={handleLogout}
-                >
-                  <LogOut className="w-5 h-5" />
-                  <span>로그아웃</span>
-                </div>
-              ) : (
-                <Link
-                  to="/sign-in" // 로그인 페이지로 이동
-                  className="flex items-center w-full gap-4 px-2 py-2"
-                >
-                  <LogOut className="w-5 h-5" />
-                  <span>로그인</span>
-                </Link>
-              )}
-            </SidebarMenuButton>
-          </SidebarMenuItem>
         </SidebarMenu>
       </SidebarFooter>
     </Sidebar>
