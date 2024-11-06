8b73682a8eae42443338fb99e240059d868c6416
soheekimdev <soheekim.dev@gmail.com>
2024-11-06 23:28:09 +0900

    Refactor: Providers를 main.tsx로 이동

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,22 @@
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from './components/theme-provider';
+import { SidebarProvider } from './components/ui/sidebar';
 import { Provider } from 'react-redux';
+import AppSidebar from './components/layout/app-sidebar';
+import App from './App.tsx';
 import store from './RTK/store.ts';
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <ThemeProvider>
+        <SidebarProvider>
+          <AppSidebar />
+          <App />
+        </SidebarProvider>
+      </ThemeProvider>
+    </BrowserRouter>
   </Provider>,
 );
