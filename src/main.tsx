6788ea1169877ae38b5219417c120bba799b724a
Ju-MINJAE <juminjae98@gmail.com>
2024-11-10 02:10:41 +0900

    Rename: rename filename

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { ThemeProvider } from './components/theme-provider';
+import { ThemeProvider } from './components/ThemeProvider.tsx';
 import { SidebarProvider } from './components/ui/sidebar';
 import { Provider } from 'react-redux';
-import AppSidebar from './components/layout/app-sidebar';
+import AppSidebar from './components/layout/AppSidebar.tsx';
 import App from './App.tsx';
 import store from './RTK/store.ts';
 import './index.css';
